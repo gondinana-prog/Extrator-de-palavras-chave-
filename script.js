6723fade308f9a9e6392f64723d0a54bdde74391
gondinana-prog <gondin.ana@escola.pr.gov.br>
2025-08-25 08:52:30 -0300

    ggg

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click , mostraPalavraschave");
+function mostraPalavraschave(){
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave= processaTexto(texto);
+    campoResultado.textContent= palavraChave.join(",");
+}
